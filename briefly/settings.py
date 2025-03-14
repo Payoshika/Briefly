cf2efa2e48b615d7a37814c39b39082d3cd6b0ee
大石航平 <oishikohei@ooishikouheis-MacBook-Pro.local>
2025-03-14 19:00:53 +0000

    db source change

--- briefly/settings.py
+++ briefly/settings.py
@@ -140,13 +140,16 @@ WSGI_APPLICATION = 'briefly.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv('DB_NAME'),
+    #     'USER': os.getenv('DB_USER'),
+    #     'PASSWORD': os.getenv('DB_PASSWORD'),
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
+    'default' :{
+        os.getenv('DB_URL')
     }
 }
 

93b3ad842cd73378efed40009094497ee902acf6
大石航平 <oishikohei@ooishikouheis-MacBook-Pro.local>
2025-03-14 18:24:26 +0000

    static files

--- briefly/settings.py
+++ briefly/settings.py
@@ -25,8 +25,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, '/templates')
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Remove the comma
 
 # Media files
 MEDIA_URL = '/media/'
